33d7a95284e264e4b71e0034efa0f7219a516fad
Gomez7373 <sgc.holberton@gmail.com>
2024-01-04 17:31:25 -0400

    updating

--- functions_monty.c
+++ functions_monty.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * pall - prints all values on the stack
+* pall - prints all values on the stack
 * @stack: pointer to the head of the stack
 * @line_number: line number in the Monty file
 *
@@ -18,6 +18,8 @@ printf("%d\n", curr->n);
 curr = curr->next;
 }
 }
+
+/*-----------------------------------------*/
 
 /**
 * push - pushes an element onto the stack
@@ -53,6 +55,8 @@ if (*stack != NULL)
 *stack = new_node;
 }
 
+/*-----------------------------------------*/
+
 /**
 * getopcode - calls the desired function to execute at the stack
 *@filename: target bytecode file
@@ -63,46 +67,48 @@ if (*stack != NULL)
 */
 void getopcode(char *filename, stack_t **stack, instruction_t *opcodes)
 {
-	char *cmd = NULL;
-	char chunk[16], number_str[32];
-	size_t len = 0;
-	unsigned int curr_line = 0, found = 0, i;
-	FILE *file = fopen(filename, "r");
+char *cmd = NULL;
+char chunk[16], number_str[32];
+size_t len = 0;
+unsigned int curr_line = 0, found = 0, i;
+FILE *file = fopen(filename, "r");
 
-	if (file == NULL)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", filename);
-		exit(EXIT_FAILURE);
-	}
-	while (getline(&cmd, &len, file) != -1)
-	{
-		number_str[0] = '\0';
-		curr_line++;
-		found = 0;
-		if (sscanf(cmd, "%s %s", chunk, number_str) < 1)
-			continue;
-		strtok(cmd, "\n");
-		strtok(chunk, " \t\n");
-		strtok(number_str, " \t\n");
-		number = atoi(number_str);
-		if (number == 0 && number_str[0] != '0')
-			number = -3694;
-		for (i = 0; opcodes[i].opcode != NULL; i++)
-		{
-			if (strcmp(chunk, opcodes[i].opcode) == 0)
-			{
-				opcodes[i].f(stack, curr_line);
-				found = 1;
-				if (*stack == NULL && strcmp(opcodes[i].opcode, "pall") != 0)
-					fail_exit(file, cmd);
-			}
-		}
-		if (!found)
-			invalidCommand(stack, file, curr_line, cmd);
-	}
-	free(cmd);
-	fclose(file);
+if (file == NULL)
+{
+fprintf(stderr, "Error: Can't open file %s\n", filename);
+exit(EXIT_FAILURE);
 }
+while (getline(&cmd, &len, file) != -1)
+{
+number_str[0] = '\0';
+curr_line++;
+found = 0;
+if (sscanf(cmd, "%s %s", chunk, number_str) < 1)
+continue;
+strtok(cmd, "\n");
+strtok(chunk, " \t\n");
+strtok(number_str, " \t\n");
+number = atoi(number_str);
+if (number == 0 && number_str[0] != '0')
+number = -3694;
+for (i = 0; opcodes[i].opcode != NULL; i++)
+{
+if (strcmp(chunk, opcodes[i].opcode) == 0)
+{
+opcodes[i].f(stack, curr_line);
+found = 1;
+if (*stack == NULL && strcmp(opcodes[i].opcode, "pall") != 0)
+fail_exit(file, cmd);
+}
+}
+if (!found)
+invalidCommand(stack, file, curr_line, cmd);
+}
+free(cmd);
+fclose(file);
+}
+
+/*-----------------------------------------*/
 
 /**
 * free_stack - Frees a stack (linked list)
@@ -123,20 +129,22 @@ current = next;
 *stack = NULL;
 }
 
+/*-----------------------------------------*/
+
 /**
- * invalidCommand - prints error when no command is found
- * @stack: the stack to free
- * @file: file to close before exit
- * @line: in what line the error happen
- * @cmd: what command was tried
- *
- * Return: void, doenst return
- */
+* invalidCommand - prints error when no command is found
+* @stack: the stack to free
+* @file: file to close before exit
+* @line: in what line the error happen
+* @cmd: what command was tried
+*
+* Return: void, doenst return
+*/
 void invalidCommand(stack_t **stack, FILE *file, unsigned int line, char *cmd)
 {
-	fprintf(stderr, "L%d: unknown instruction %s\n", line, cmd);
-	free_stack(stack);
-	fclose(file);
-	free(cmd);
-	exit(EXIT_FAILURE);
+fprintf(stderr, "L%d: unknown instruction %s\n", line, cmd);
+free_stack(stack);
+fclose(file);
+free(cmd);
+exit(EXIT_FAILURE);
 }
